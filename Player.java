ce28a0882e944aa11b0c9c80a6370d02b1e0e628
thl024 <lamtimothy1996@gmail.com>
2015-06-20 17:18:01 -0700

    Initial Player Creation
    Unfinished abstract class for inheritance between card games.

---
+++ Player.java
@@ -0,0 +1,14 @@
+package cardsuite;
+import java.util.*;
+
+// UNFINISHED
+
+public abstract class Player {
+	
+	protected List<Card> hand;
+	protected int score;
+	
+	public abstract void fillHand();
+	public abstract void scoreChange();
+	
+}
