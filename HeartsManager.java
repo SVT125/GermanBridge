efc954e11e33631bfd2e023fc6defecde5645f58
thl024 <lamtimothy1996@gmail.com>
2015-06-21 11:52:56 -0700

    Added Swap Method
    May contain a few type related errors. Might also be ways to condense code.

--- HeartsManager.java
+++ HeartsManager.java
@@ -28,8 +28,44 @@ public class HeartsManager {
 		
 	}
 	
-	public void swapCards(Player[] players, Card[] chosen) {
+	public void swapCards(Player[] players, Collection<?> chosen, int playerNum) {
 		
+		// player 2 is to the left of player 1
+		// might have a few errors
+		switch (roundCount % 4) {
+			case 0:
+				if (playerNum != 3) {
+					players[playerNum].hand.removeAll(chosen);
+					players[playerNum + 1].hand.addAll((Collection<? extends Card>) chosen);
+				}
+				else {
+					players[playerNum].hand.removeAll(chosen);
+					players[0].hand.addAll((Collection<? extends Card>) chosen);
+				}
+				break;
+			case 1:
+				if (playerNum != 0) {
+					players[playerNum].hand.removeAll(chosen);
+					players[playerNum - 1].hand.addAll((Collection<? extends Card>) chosen);
+				}
+				else {
+					players[playerNum].hand.removeAll(chosen);
+					players[3].hand.addAll((Collection<? extends Card>) chosen);
+				}
+				break;
+			case 2:
+				if (playerNum == 0 || playerNum == 1) {
+					players[playerNum].hand.removeAll(chosen);
+					players[playerNum + 2].hand.addAll((Collection<? extends Card>) chosen);
+				}
+				else {
+					players[playerNum].hand.removeAll(chosen);
+					players[playerNum - 2].hand.addAll((Collection<? extends Card>) chosen);
+				}
+				break;
+			case 3:
+				break;
+		}
 	}
 	
 }
