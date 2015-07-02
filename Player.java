6f3ce9be01a3f9b6259d8011b4991a35a417d350
thl024 <lamtimothy1996@gmail.com>
2015-07-01 19:27:08 -0700

    Ported methods from child class

--- Player.java
+++ Player.java
@@ -7,9 +7,38 @@ public abstract class Player {
 	protected List<Card> hand;
 	protected int score;
 	
-	public abstract ArrayList<Card> fillHand(ArrayList<Card> deck, Random random);
 	public abstract void scoreChange();
-	public abstract void organize();
-	public abstract boolean hasSuit(Card.Suit suit);
+	
+	public void organize() {
+		Collections.sort(hand, new CardComparator());
+	}
+	
+	public class CardComparator implements Comparator<Card> {
+		
+		@Override
+		public int compare(Card a, Card b) {
+			if (a.getSuit().equals(b.getSuit())) {
+				return a.getCardNumber() - b.getCardNumber();
+			}
+			return a.getSuit().compareTo(b.getSuit());	
+		}
+		
+	}
+	
+	public ArrayList<Card> fillHand(ArrayList<Card> deck, Random random, int size) {
+		for (int i = 0; i < size; i++) {
+			hand.add(deck.remove(random.nextInt(deck.size())));
+		}
+		return deck;
+	}
+	
+	public boolean hasSuit(Card.Suit suit) {
+		for (Card card : hand) {
+			if (card.getSuit() == suit) {
+				return true;
+			}
+		}
+		return false;
+	}
 	
 }
