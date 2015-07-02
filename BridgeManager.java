695162dc2cf9d67cb060108f087ca148c00e0b2a
thl024 <lamtimothy1996@gmail.com>
2015-07-01 22:43:02 -0700

    Update 1.1

--- BridgeManager.java
+++ BridgeManager.java
@@ -1,7 +1,27 @@
 package cardsuite;
 
-public class BridgeManager {
+public class BridgeManager extends Manager {
 	
+	public BridgeManager() {
+		playerCount = 4;
+		players = new HeartsPlayer[playerCount];
+		
+		for (int i = 2; i < 15; i++) {
+			for (Card.Suit suits : Card.Suit.values()) {
+				deck.add(new Card(i, suits));
+			}
+		}
+		
+		for (int i = 0; i < playerCount; i++) {
+			players[i] = new HeartsPlayer();
+			deck = players[i].fillHand(deck, random, roundCount);
+		}
+	}
 	
+	public static void main(String args[]) {
+		
+		
+		
+	}
 
 }
