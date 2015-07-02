512e4d4cbe528021fc98429f597e4ad11269e961
thl024 <lamtimothy1996@gmail.com>
2015-07-01 22:44:17 -0700

    Update 1.1

--- BridgePlayer.java
+++ BridgePlayer.java
@@ -5,6 +5,13 @@ import java.util.Random;
 
 public class BridgePlayer extends Player {
 
+	public BridgePlayer() {
+		hand = new ArrayList<Card>();
+		this.score = 0;
+	}
 	
+	public void scoreChange() {
+		
+	}
 	
 }
