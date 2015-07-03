0bea59b79074fc2d7fb245bc603a3993e0cbcee2
thl024 <lamtimothy1996@gmail.com>
2015-07-03 12:53:17 -0700

    Initial Spaders Player

---
+++ SpadesPlayer.java
@@ -0,0 +1,16 @@
+package cardsuite;
+
+public class SpadesPlayer extends Player {
+	
+	protected int bid;
+	protected int partnerBid;
+	protected int totalBid;
+	protected int bags = 0;
+	protected int bagLimit = 10;
+	protected int obtained = 0;
+	
+	public void scoreChange() {
+		
+	}
+	
+}
