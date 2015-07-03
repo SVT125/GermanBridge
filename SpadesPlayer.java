015eaacc749830a3d743ca225148223f5bad7ca6
thl024 <lamtimothy1996@gmail.com>
2015-07-03 14:45:56 -0700

    Added Constructor

--- SpadesPlayer.java
+++ SpadesPlayer.java
@@ -1,5 +1,7 @@
 package cardsuite;
 
+import java.util.ArrayList;
+
 public class SpadesPlayer extends Player {
 	
 	protected int bid;
@@ -8,6 +10,11 @@ public class SpadesPlayer extends Player {
 	protected int bags = 0;
 	protected int bagLimit = 10;
 	protected int obtained = 0;
+	
+	public SpadesPlayer() {
+		hand = new ArrayList<Card>();
+		this.score = 0;
+	}
 	
 	public void scoreChange() {
 		
