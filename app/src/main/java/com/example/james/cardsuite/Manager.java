e078e9c763ab860081535d25336e8b45ecb60c64
thl024 <lamtimothy1996@gmail.com>
2015-08-10 13:19:43 -0700

    Updated abstract class to hold all managers

--- app/src/main/java/com/example/james/cardsuite/Manager.java
+++ app/src/main/java/com/example/james/cardsuite/Manager.java
@@ -1,24 +1,30 @@
 package com.example.james.cardsuite;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
-import java.util.Scanner;
+
+import android.widget.TextView;
 
 public abstract class Manager {
 	
 	protected ArrayList<Card> deck = new ArrayList<Card>();
-	protected Random random = new Random();
-	protected int playerCount;
-	protected int potsFinished = 1;
-	protected static Scanner scanner = new Scanner(System.in);
 	protected Map<Integer, Card> pot;
 	protected Card.Suit startSuit;
-	protected int startPlayer = 0;
+	protected Random random = new Random();
+	protected int startPlayer = 0, totalRoundCount, playerCount, potsFinished = 1, addedGuesses;
 	protected Player[] players;
 
 	public abstract void potAnalyze();
+	public abstract boolean potHandle(TextView output, int chosen, int currentPlayer, boolean initialOutputWritten, GameActivity activity);
 	public abstract void reset();
+	public abstract boolean isGameOver();
+	public abstract boolean cardSelectable(Card card, boolean finishedSwapping, int currentPlayer);
+	public abstract List<Card> chooseCards(int playerNum, List<Integer> chosenIndices);
+	public abstract void swapCards(Collection<?> chosen, int playerNum, int swapRound);
+	public abstract int findStartPlayer();
 	public int getPotsFinished() {
 		return this.potsFinished;
 	}
