58929c67ca77a121f9f15461583d51b4a3de9580
thl024 <lamtimothy1996@gmail.com>
2015-08-10 12:37:40 -0700

    Updated SpadesManager to Android

--- app/src/main/java/com/example/james/cardsuite/SpadesManager.java
+++ app/src/main/java/com/example/james/cardsuite/SpadesManager.java
@@ -1,11 +1,15 @@
 package com.example.james.cardsuite;
 
+import android.app.Activity;
+import android.widget.TextView;
+
+import java.awt.font.TextAttribute;
 import java.util.Map.Entry;
 
 public class SpadesManager extends Manager {
 
 	private boolean spadesBroken = false;
-	private SpadesPlayer[] players;
+	public SpadesPlayer[] players;
 	
 	public SpadesManager() {
 		
@@ -27,23 +31,16 @@ public class SpadesManager extends Manager {
 		}
 		
 	}
-	
-	public static void main(String args[]) {
-		
-		SpadesManager manager=  new SpadesManager();
-		
-		manager.bid();
-		
-		for (int i = 0; i < manager.potsFinished; i++) {
-			manager.potHandle();
+
+	// not sure if this is correct
+	public boolean isGameOver() {
+		for(Player player : players) {
+			if (player.score >= 500)
+				return true;
+			if (player.score <= -200)
+				return false;
 		}
-		
-		for (SpadesPlayer player : manager.players) {
-			player.scoreChange();
-		}
-		
-		manager.reset();
-		
+		return false;
 	}
 	
 	public void reset() {
@@ -62,55 +59,45 @@ public class SpadesManager extends Manager {
 			deck = players[i].fillHand(deck, random, 13);
 		}
 	}
-	
-	public void potHandle() {
-		
-		Card startCard;
-		int chosen;
-		
-		// first player move; cannot place spades
-		do {
-			chosen = scanner.nextInt();
-		} while (players[startPlayer].hand.get(chosen).getSuit() == Card.Suit.SPADES);
-		
-		startCard = players[startPlayer].hand.remove(chosen);
-		int currentPlayer = startPlayer;
-		pot.put(startPlayer,startCard);
-		startSuit = startCard.getSuit();
-		
-		for (int i = 0; i < 3; i++) {
-			currentPlayer++;
-			if (currentPlayer == playerCount) {
-				currentPlayer = 0;
-			}
-			
-			// must place down a card of the same suit
-			if (players[currentPlayer].hasSuit(startSuit)) {
-				do {
-					chosen = scanner.nextInt();
-				} while (players[startPlayer].hand.get(chosen).getSuit() != startSuit);
-			}
-			
-			// if first turn and has none of same card, cannot place spades
-			else if (players[currentPlayer].hand.size() == 13) {
-				do {
-					chosen = scanner.nextInt();
-				} while (players[startPlayer].hand.get(chosen).getSuit() == Card.Suit.SPADES);
-			}
-			
-			// otherwise can place anything
-			else {
-				chosen = scanner.nextInt();
-				if (players[startPlayer].hand.get(chosen).getSuit() == Card.Suit.SPADES && !spadesBroken) {
-					spadesBroken = true;
-				}
-			}
-			
-			pot.put(currentPlayer, players[currentPlayer].hand.remove(chosen));
+
+	public boolean potHandle(TextView output, int chosen, int currentPlayer, boolean initialOutputWritten, GameActivity activity) {
+		if (!initialOutputWritten) {
+			this.writeOutput(currentPlayer, output, activity);
+			activity.initialOutputWritten = true;
+			return false;
 		}
-		
-		potAnalyze();
-		
+
+		Card selectCard = players[currentPlayer].hand.remove(chosen);
+
+		if(currentPlayer == startPlayer)
+			startSuit = selectCard.getSuit();
+		else if (!spadesBroken && (selectCard.getSuit().equals(Card.Suit.SPADES)))
+			spadesBroken = true;
+
+		pot.put(currentPlayer, selectCard);
+		this.writeOutput((currentPlayer + 1) % 4, output, activity);
+		return true;
+	}
+
+	public boolean cardSelectable(Card card, int currentPlayer) {
+		if ((players[startPlayer].hand.size() == 13) && (card.compareTo(new Card(2, Card.Suit.CLUBS)) != 0)) {
+			return false;
+		}
+		if (currentPlayer == startPlayer && (!spadesBroken && (card.getSuit().equals(Card.Suit.HEARTS)) || card.compareTo(new Card(12, Card.Suit.SPADES)) == 0)) {
+			return false;
+		}
+		if (players[currentPlayer].hasSuit(startSuit) && pot.size() != 0) {
+			if (!(card.getSuit().equals(startSuit))) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public void writeOutput(int currentPlayer, TextView output, GameActivity activity) {
+		players[currentPlayer].organize();
+		activity.displayHands(currentPlayer);
+		output.setText("Player " + Integer.toString(currentPlayer + 1) + " place a card");
 	}
 	
 	public void potAnalyze() {
@@ -173,5 +160,4 @@ public class SpadesManager extends Manager {
 			players[i].partnerBid = players[(i+2)%4].bid;
 		
 	}
-	
 }
