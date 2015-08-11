c1f3b34278929a15594169837306238d59396f8e
thl024 <lamtimothy1996@gmail.com>
2015-08-10 18:17:17 -0700

    Added Spades Scoring

--- app/src/main/java/com/example/james/cardsuite/SpadesPlayer.java
+++ app/src/main/java/com/example/james/cardsuite/SpadesPlayer.java
@@ -4,12 +4,9 @@ import java.util.ArrayList;
 
 public class SpadesPlayer extends Player {
 	
-	protected int bid;
-	protected int partnerBid;
-	protected int totalBid;
-	protected int bags = 0;
-	protected int obtained = 0;
-	protected int totalObtained = 0;
+	protected int bid, totalBid, bags, obtained, totalObtained;
+	protected boolean blindNil = false;
+	protected SpadesPlayer partner;
 	
 	public SpadesPlayer() {
 		hand = new ArrayList<Card>();
@@ -18,29 +15,24 @@ public class SpadesPlayer extends Player {
 	
 	// PROBABLY VERY ERROR PRONE; BUG FIXES PROBABLY NECESSARY
 	public boolean scoreChange() {
-		totalBid = bid + partnerBid;
-		
-		// if nil bid is obtained by any person, team gets 100 points
-		if (obtained == 0 && (bid == 0 || partnerBid == 0)) {
-			score = score + 100;
+
+		// if person gets 0 hands
+		if (obtained == 0) {
+			if (blindNil) { this.score += 200; partner.score += 200; }
+			else if (bid == 0) { this.score += 100; partner.score += 100; }
 		}
-		
-		if (totalBid >= totalObtained) {
-			// score if guessed right increases by ten times the total bid plus one time each obtained over
-			score = score + totalBid * 10 + (totalBid - totalObtained);
-			// bags increase for each one over that the team guesses
-			bags = bags + (totalBid - totalObtained);
-			// if bags reach ten, score deducted by 100
-			if (bags >= 10) {
-				bags = bags - 10;
-				score = score - 100;
+		else {
+			if (totalBid > totalObtained) { this.score -= (10 * totalBid); }
+			else {
+				this.score += (10 * totalBid);
+				this.bags += (totalObtained - totalBid);
 			}
 		}
-		// otherwise, team loses 10 points for each miss
-		else {
-			score = score - (totalBid - totalObtained) * 10;
+		if (this.bags >= 10) {
+			this.bags -= 10;
+			this.score -= 100;
 		}
+
 		return false;
 	}
-	
 }
