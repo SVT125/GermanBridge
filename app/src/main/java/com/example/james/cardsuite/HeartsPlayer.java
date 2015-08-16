e42a4cc802f4e16cf794e6a4829efc6a92c35805
thl024 <lamtimothy1996@gmail.com>
2015-08-15 18:23:04 -0700

    Added TallyRoundScore

--- app/src/main/java/com/example/james/cardsuite/HeartsPlayer.java
+++ app/src/main/java/com/example/james/cardsuite/HeartsPlayer.java
@@ -6,6 +6,7 @@ public class HeartsPlayer extends Player implements Serializable {
 	
 	// added endPile to count scores of each player at the end of every round
 	protected List<Card> endPile;
+	protected int roundScore = 0;
 	
 	public HeartsPlayer() {
 		hand = new ArrayList<Card>();
@@ -14,24 +15,25 @@ public class HeartsPlayer extends Player implements Serializable {
 
 	// boolean returns true or false value depending on whether player has shot the moon or not
 	public boolean scoreChange() {
-		int roundScore = 0;
+		if (roundScore == 26) {
+			return true;
+		}
+		this.score += roundScore;
+		roundScore = 0;
+		return false;
+	}
+
+	public int tallyRoundScore() {
 		for (Card card : endPile) {
 			if (card.getSuit() == Card.Suit.HEARTS) {
-				this.score++;
 				roundScore++;
 			}
 			else if (card.compareTo(new Card(12, Card.Suit.SPADES)) == 0) {
-				this.score += 13;
 				roundScore += 13;
 			}
 		}
 		endPile.clear();
-		if (roundScore == 26) {
-			this.score -= 26;
-			return true;
-		}
-		return false;
+		return roundScore;
 	}
-	
 	
 }
