b795ccac0ee251721c8a9f4bc561ddb543d34c21
SVT125 <Jhardjadinata14@gmail.com>
2015-08-18 10:41:34 -0700

    Initial push for bridge's MaxN method

--- app/src/main/java/com/example/james/cardsuite/BridgeAI.java
+++ app/src/main/java/com/example/james/cardsuite/BridgeAI.java
@@ -1,8 +1,30 @@
 package com.example.james.cardsuite;
 
-//The German Bridge AI, heuristic eval function sourced mostly from http://www.fongboy.com/programs/Whist/whist_paper.pdf with Luckhardt/Irani's MaxN algorithm.
+// The German Bridge AI, heuristic eval function sourced mostly from http://www.fongboy.com/programs/Whist/whist_paper.pdf with Luckhardt/Irani's MaxN algorithm.
+// Will also consider http://www.cs.umd.edu/~bswilson/presentation.pdf for socially oriented evaluations and varying difficulties.
 public class BridgeAI {
-    //Given the manager, which represents the current state of the game, and the player of interest, return their heuristic value.
+    // The maxN recursive function with shallow pruning.
+    // Returns a 4-tuple for index ij, where i is the ith index of the tuple and j is the jth node of the tree.
+    private static double[] maxN(int currentPlayer, BridgeManager manager, int level, int turnsLeft) {
+        // If we've hit a terminal node or don't wish to continue searching, return the evaluation vector for the node.
+        if(turnsLeft == 0 || level == 0)
+            return new double[] {evaluate(0, manager), evaluate(1, manager),
+                    evaluate(2,manager), evaluate(3,manager)};
+        else {
+            // Otherwise, Evaluate maxN for all child nodes and return the max possible vector.
+            double[] maxVector = new double[] {Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE};
+
+            for(int i = 0; i < manager.players[i].hand.size(); i++) {
+                manager.players[i].hand.remove(i);
+                double[] evaluationVector = maxN((currentPlayer+1)%4,manager,level-1,turnsLeft-1);
+                if(evaluationVector[currentPlayer-1] > maxVector[currentPlayer-1])
+                    maxVector = evaluationVector;
+            }
+            return maxVector;
+        }
+    }
+
+    // Given the manager, which represents the current state of the game, and the player of interest, return their heuristic value.
     private static double evaluate(int currentPlayer, BridgeManager manager) {
         BridgePlayer player = ((BridgePlayer)manager.players[currentPlayer]);
         double partialTricksRemaining = player.guess - player.obtained - 0.75 * findGuaranteedWinners(currentPlayer,manager), tricksRemaining;
@@ -19,8 +41,8 @@ public class BridgeAI {
         return pointBidEstimate + pointTricksEstimate + guessAllMissingBids(manager);
     }
 
-    //Returns the number of opponents who we guess will miss their bids.
-    //Each opponent is guessed to miss their bid if their tricks remaining value is less than -2 after considering all categories.
+    // Returns the number of opponents who we guess will miss their bids.
+    // Each opponent is guessed to miss their bid if their tricks remaining value is less than -2 after considering all categories.
     private static int guessAllMissingBids(BridgeManager manager) {
         int missedBids = 0;
         for(int i = 0; i < manager.players.length; i++) {
@@ -38,8 +60,8 @@ public class BridgeAI {
         return missedBids;
     }
 
-    //Returns the number of guaranteed winners in the given player's hand.
-    //Guaranteed winner cards are defined here as aces of the trump suit (therefore values 0-1).
+    // Returns the number of guaranteed winners in the given player's hand.
+    // Guaranteed winner cards are defined here as aces of the trump suit (therefore values 0-1).
     private static int findGuaranteedWinners(int currentPlayer, BridgeManager manager) {
         int found = 0;
         for(Card card : ((BridgePlayer)manager.players[currentPlayer]).hand)
@@ -49,8 +71,8 @@ public class BridgeAI {
         return found;
     }
 
-    //Returns the number of guaranteed winners in the given player's hand.
-    //Guaranteed winner cards are defined here as 2's of the trump suit (therefore values 0-1).
+    // Returns the number of guaranteed winners in the given player's hand.
+    // Guaranteed winner cards are defined here as 2's of the trump suit (therefore values 0-1).
     private static int findGuaranteedLosers(int currentPlayer, BridgeManager manager) {
         int found = 0;
         for(Card card : ((BridgePlayer)manager.players[currentPlayer]).hand)
@@ -60,8 +82,8 @@ public class BridgeAI {
         return found;
     }
 
-    //Returns the number of guaranteed winners in the given player's hand.
-    //Likely loser cards are defined here as 10-15's of the trump suit.
+    // Returns the number of guaranteed winners in the given player's hand.
+    // Likely loser cards are defined here as 10-15's of the trump suit.
     private static int findLikelyWinners(int currentPlayer, BridgeManager manager) {
         int found = 0;
         for(Card card : ((BridgePlayer)manager.players[currentPlayer]).hand)
@@ -71,8 +93,8 @@ public class BridgeAI {
         return found;
     }
 
-    //Returns the number of guaranteed winners in the given player's hand.
-    //Likely loser cards are defined here as 2-7's of the trump suit.
+    // Returns the number of guaranteed winners in the given player's hand.
+    // Likely loser cards are defined here as 2-7's of the trump suit.
     private static int findLikelyLosers(int currentPlayer, BridgeManager manager) {
         int found = 0;
         for(Card card : ((BridgePlayer)manager.players[currentPlayer]).hand)
@@ -82,8 +104,8 @@ public class BridgeAI {
         return found;
     }
 
-    //Returns the number of guaranteed winners in the given player's hand.
-    //Neutral cards are defined here as 7-10's of the trump suit.
+    // Returns the number of guaranteed winners in the given player's hand.
+    // Neutral cards are defined here as 7-10's of the trump suit.
     private static int findNeutrals(int currentPlayer, BridgeManager manager) {
         int found = 0;
         for(Card card : ((BridgePlayer)manager.players[currentPlayer]).hand)
