48986d2470c1bc388a2d1009a73351db2cfaf941
SVT125 <Jhardjadinata14@gmail.com>
2015-08-27 17:04:40 -0700

    Readded swapping select animations

--- app/src/main/java/com/example/james/cardsuite/HeartsActivity.java
+++ app/src/main/java/com/example/james/cardsuite/HeartsActivity.java
@@ -1,5 +1,7 @@
 package com.example.james.cardsuite;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Color;
@@ -200,6 +202,28 @@ public class HeartsActivity extends GameActivity {
     public void chooseCards(Card chosenCard, int swapRound, View v) {
         if (chosenCard.isClicked == false) {
             chosenCard.isClicked = true;
+            switch(currentPlayerInteracting){
+                case 0:
+                    AnimatorSet selected_f = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.anim.selected_forward);
+                    selected_f.setTarget(v);
+                    selected_f.start();
+                    break;
+                case 1:
+                    AnimatorSet selected_r = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.anim.selected_right);
+                    selected_r.setTarget(v);
+                    selected_r.start();
+                    break;
+                case 2:
+                    AnimatorSet selected_b = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.anim.selected_back);
+                    selected_b.setTarget(v);
+                    selected_b.start();
+                    break;
+                case 3:
+                    AnimatorSet selected_l = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.anim.selected_left);
+                    selected_l.setTarget(v);
+                    selected_l.start();
+                    break;
+            }
         } else {
             v.setBackgroundResource(0);
             chosenCard.isClicked = false;
